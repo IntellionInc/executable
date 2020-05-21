8178d96246c0cfffeb7f28ccea6ff9a3ff060432
Cefi Menda <jeffymenda@gmail.com>
2020-05-21 02:05:26 -0400

    add executableMatcha shorthands

--- test/ExecutableMatcha.js
+++ test/ExecutableMatcha.js
@@ -1,6 +1,16 @@
 let { Assertion, Stub, expect, sinon } = require("@intellion/matchalatte");
 
-class ExecutableAssertion extends Assertion { };
+class ExecutableAssertion extends Assertion {
+  receivesBeforeHook = hookMethod => {
+    hookMethod();
+    return this;
+  };
+  whenBeforeHookIsCalled = index => {
+    this._obj = this._obj._beforeHooks[index || 0];
+    this.args = [];
+    return this;
+  };
+};
 class ExecutableStub extends Stub { };
 
 module.exports = {
